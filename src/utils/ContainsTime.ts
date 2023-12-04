3d1e4532b741aa5f1ee184c45de3201dff051999
bsgriggs <benjaminsgriggs@gmail.com>
2023-12-04 09:42:49 -0500

    init

--- src/utils/ContainsTime.ts
+++
@@ -1,9 +0,0 @@
-const ContainsTime = (dateFormat: string): boolean =>
-    dateFormat.includes("H") ||
-    dateFormat.includes("h") ||
-    dateFormat.includes("m") ||
-    dateFormat.includes("s") ||
-    dateFormat.includes("z") ||
-    dateFormat.includes("a");
-
-export default ContainsTime;
--- src/utils/MxFormatter.ts
+++
@@ -1,7 +0,0 @@
-const MxFormatter = (date: Date, dateFormat: string): string => {
-    /* eslint-disable */
-    // @ts-ignore
-    return mx.parser.formatValue(date, "datetime", { datePattern: dateFormat });
-    /* eslint-enable */
-};
-export default MxFormatter;
